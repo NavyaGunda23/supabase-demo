8a99ecbc1a8f5aa005bc72bdeedcf899f93b7c8b
NavyaGunda23 <navyagunda.71@gmail.com>
2025-06-23 13:01:07 +0400

    psuging fikles

---
+++ app/api/message/route.ts
@@ -0,0 +1,22 @@
+// app/api/message/route.ts
+import { NextResponse } from 'next/server';
+
+let data = { record: { email: "test12@gmail.com" } };
+
+export async function GET(request:Request) {
+  return NextResponse.json({
+    status: 200,
+    data,
+  });
+}
+
+export async function POST(request: Request) {
+  const req = await request.json();
+  data = req;
+  console.log("📨 Webhook triggered with payload:", req); // ✅ log here
+  return NextResponse.json({
+    status: 200,
+    data,
+    message: "Data has been updated",
+  });
+}
